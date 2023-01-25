921d5044d298e95e3cfcbb2eb98708800997540e
cjaybond <jamesuchennachi@gmail.com>
2023-01-25 01:29:04 +0100

    Implemented Registration and login endpoints

---
+++ PaylodeWeather/PaylodeWeather.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaylodeWeather.Core.Dtos;
+using PaylodeWeather.Core.Interfaces;
+using System.Net.Mime;
+
+namespace PaylodeWeather.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationDTO model)
+        {
+            var result = await _authService.Register(model);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO model)
+        {
+            var response = await _authService.Login(model);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
